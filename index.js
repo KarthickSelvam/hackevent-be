2d4d278a2e1f838b4cfe98aacadb9dd0b527d4db
ShaulHameed <shaul.hameed@gyanmatrix.com>
2019-08-03 18:31:17 +0530

    enabled https

--- index.js
+++ index.js
@@ -1,34 +1,42 @@
 const express = require('express');
 const app = express();
-const http = require('http').Server(app);
 const bodyParser = require('body-parser');
 const helmet = require('helmet');
 const cors = require('cors');
-const io = require('socket.io')(http);
-// Custom dependency.
-//const config = require('./server/config');
-// global.db = global.db ? global.db : require('./server/data/db')();
+const fs = require('fs');
+const https = require('https');
+const io = require('socket.io')(https);
+const dotenv = require('dotenv');
+
 
 
 // Application middleware goes here.
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json()); // parse application/json
 app.use(helmet.hidePoweredBy());
-// Token validation is done in the API gateway itself.
-// app.all(`${config.apiBasePath}/*`, auth.validateToken);
 
-// const corsOptions = {
-//     exposedHeaders: ['X-Total', 'X-TotalPages'],
-//   };
-//   app.use(cors(corsOptions));
+
+//load the environment config.
+dotenv.config();
+
+
+//default endpoint
+app.get('/', (req, res) => {
+
+    res.send("Hackevent-BE");
+
+})
+
 
 io.on('connection', socket => {
     //When a connection was created.
     console.log('new connectiion');
-    
 });
 
-http.listen(3001, () => {
-    console.log('start', `Listening on port 3001...`);
-  });
-
+https.createServer({
+        key: fs.readFileSync('./cert/server.key'),
+        cert: fs.readFileSync('./cert/server.cert')
+    }, app)
+    .listen(process.env.HTTPS_PORT, () => {
+        console.log("HTTPS Server is listening on port %s", process.env.HTTPS_PORT)
+    });
