92393cb9adafab64274984cb19dd571c2e87b599
Karthick S <karthick.selvam@globalenglish.com>
2019-08-03 20:13:47 +0530

    socket base setup WIP

--- index.js
+++ index.js
@@ -1,12 +1,15 @@
 const express = require('express');
+const app = express();
+const http = require('http').Server(app);
 const bodyParser = require('body-parser');
 const passport = require('passport');
 const config = require('./config');
+const io = require('socket.io')(http);
+
 
 // connect to the database and load models
 require('./server/models').connect(config.dbUri);
 
-const app = express();
 // tell the app to look for static files in these directories
 //app.use(express.static('./server/static/'));
 //app.use(express.static('./client/dist/'));
@@ -31,8 +34,52 @@ const apiRoutes = require('./server/routes/api');
 app.use('/auth', authRoutes);
 app.use('/api', apiRoutes);
 
+//socket-io 
+const ClientManager = require('./server/socket/ClientManager');
+const ChatroomManager = require('./server/socket/ChatroomManager');
+const makeHandlers = require('./server/socket/handlers');
+
+const clientManager = ClientManager();
+const chatroomManager = ChatroomManager();
+
+io.on('connection', function (client) {
+  const {
+    handleRegister,
+    handleJoin,
+    handleLeave,
+    handleMessage,
+    handleGetChatrooms,
+    handleGetAvailableUsers,
+    handleDisconnect
+  } = makeHandlers(client, clientManager, chatroomManager)
+
+  console.log('client connected...', client.id)
+  clientManager.addClient(client)
+
+  client.on('register', handleRegister)
+
+  client.on('join', handleJoin)
+
+  client.on('leave', handleLeave)
+
+  client.on('message', handleMessage)
+
+  client.on('chatrooms', handleGetChatrooms)
+
+  client.on('availableUsers', handleGetAvailableUsers)
+
+  client.on('disconnect', function () {
+    console.log('client disconnect...', client.id)
+    handleDisconnect()
+  })
+
+  client.on('error', function (err) {
+    console.log('received error from client:', client.id)
+    console.log(err)
+  })
+});
 // Set Port, hosting services will look for process.env.PORT
-app.set('port', (process.env.PORT || 3000));
+app.set('port', (process.env.PORT || 3001));
 
 // start the server
 app.listen(app.get('port'), () => {
